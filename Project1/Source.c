9dca2562f5b3dc4a34ac22aff7a1155421813ac1
Maxim (Student of Voronezh State Technical University) <63464684+KeksikProg@users.noreply.github.com>
2022-12-19 20:07:02 +0300

    make functions prototips

--- Project1/Source.c
+++ Project1/Source.c
@@ -7,206 +7,22 @@
 
 #define _USE_MATH_DEFINES
 
+//functions
+void id_team(int ptr_array[12]);
+void num_quest(int ptr_array[12][15]);
+void num_time(int ptr_array[12][15]);
+void num_volume(int ptr_array[12][15]);
 
+int num_score(int ptr_array[12][15], int index);
 
-// Functions for add elements in array
-void id_team(int ptr_array[12]) {
-	for (int i = 0; i < 12; i++) {
-		ptr_array[i] = 10000 + rand() % (99999 - 10000 + 1);
-	}
-}
+void table1_on_screen(int ptr_array[12][15]);
+void table2_on_screen(int ptr_array[12][15]);
 
-void num_quest(int ptr_array[12][15]) {
-	for (int i = 1; i < 13; i++) {
-		for (int j = 0; j < 14; j++) {
-			ptr_array[i][j] = rand() % 2;
-		}
-	}
-}
+int find_quest(int ptr_array[12][15]);
 
-void num_time(int ptr_array[12][15]) {
-	for (int i = 0; i < 12; i++) {
-		ptr_array[13][i] = rand() % 300;
-	}
-}
-
-void num_volume(int ptr_array[12][15]) {
-	for (int i = 0; i < 12; i++) {
-		ptr_array[14][i] = rand() % 100;
-	}
-}
+void sort_2d_array(int ptr_array[12][15]);
+FILE* input_from_file(int ptr_array[12][15]);
 //
-
-//function for sum score for teams
-int num_score(int ptr_array[12][15], int index) {
-	int id_team = ptr_array[index], num_score = 0, time_score;
-
-	for (int i = 1; i < 13; i++) {
-		num_score += ptr_array[i][index];
-	}
-
-	time_score = ptr_array[13][index] / 60;
-	num_score += time_score;
-
-	return num_score;
-}
-//
-
-
-//function for table on screen
-void table1_on_screen(int ptr_array[12][15]) {
-	printf("Id команды Зад.1 Зад.2 Зад.3 Зад.4   Зад.5 Зад.6  Зад.7   Зад.8   Зад.9 Зад.10 Зад.11 Зад.12 Ост.время Исп.памяти");
-	for (int i = 0; i < 12; i++) { //nlines
-		printf("\n");
-		for (int j = 0; j < 15; j++) { //ncols
-			if (j != 13) {
-				printf("%6d ", ptr_array[j][i]);//i * ncols + j
-			}
-			else {
-				if (ptr_array[j][i] % 60 > 9) {
-					printf("   0%d:%2d", ptr_array[j][i] / 60, ptr_array[j][i] % 60);
-				}
-				else {
-					printf("   0%d:0%d", ptr_array[j][i] / 60, ptr_array[j][i] % 60);
-				}
-			}
-		}
-	}
-	printf("\n");
-}
-
-void table2_on_screen(int ptr_array[12][15]) {
-	printf("Id команды   Кол-во баллов\n");
-	for (int i = 0; i < 12; i++) {
-		printf("%6d              %2d\n", ptr_array[0][i], num_score(ptr_array, i));
-	}
-}
-//
-
-
-//function for find tasks
-int find_quest(int ptr_array[12][15]) {
-	int num_quest, result;
-
-	printf("Введите номер задачи, по которой хотите произвести поиск (1-12): ");
-	scanf_s("%d", &num_quest);
-	printf("Была ли решена задача? (введите номер ответа):\n 1. Да \n 0. Нет\n");
-	scanf_s("%d", &result);
-
-	system("cls");
-	printf("Id команды Зад.1 Зад.2 Зад.3 Зад.4   Зад.5 Зад.6  Зад.7   Зад.8   Зад.9 Зад.10 Зад.11 Зад.12 Исп.памяти   Ост.время\n");
-	for (int i = 0; i < 12; i++) {
-
-			if (ptr_array[num_quest][i] == result) {
-				printf("%6d", ptr_array[0][i]);
-
-				for (int k = 1; k < 13; k++) {
-					printf("%6d ", ptr_array[k][i]);
-				}
-
-				printf("  %6d", ptr_array[13][i]);
-
-				if (ptr_array[14][i] % 60 > 9) {
-					printf("          0%d:%2d", ptr_array[14][i] / 60, ptr_array[14][i] % 60);
-
-				}
-				else {
-					printf("          0%d:0%0d", ptr_array[14][i] / 60, ptr_array[14][i] % 60);
-				}
-				printf("\n");
-			}
-			else {
-				continue;
-			}
-
-		
-		
-	}
-	printf("\n\n");
-}
-//
-
-//function for sort 2d array
-void sort_2d_array(int ptr_array[12][15]) {
-	int tmp;
-	int choose;
-
-	printf("Сортировка:\n1. По возрастанию\n2. По убыванию\n");
-	scanf_s("%d", &choose);
-
-	if (choose == 1) {
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 12; j++) {
-				if (ptr_array[13][j] > ptr_array[13][j+1]) {
-					for (int k = 0; k < 15; k++) {
-						tmp = ptr_array[k][j];
-						ptr_array[k][j] = ptr_array[k][j + 1];
-						ptr_array[k][j + 1] = tmp;
-					}
-				}
-			}
-		}
-	}
-	if (choose == 2) {
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 11; j++) {
-				if (ptr_array[13][j] < ptr_array[13][j + 1]) {
-					for (int k = 0; k < 15; k++) {
-						tmp = ptr_array[k][j];
-						ptr_array[k][j] = ptr_array[k][j + 1];
-						ptr_array[k][j + 1] = tmp;
-					}
-				}
-			}
-		}
-	}
-}
-
-FILE* input_from_file(int ptr_array[12][15]) {
-	char  extension[6] = { ".txt\0" };
-	char file_name[40]="data";
-	int size = 0;
-	FILE* table_l;
-
-
-	puts("Введите название файла в пределах 30 символов (без указания расширения файла)");
-	scanf("%s", &file_name);
-	strcat(file_name, extension);
-	printf("Имя файла: %s\n", file_name);
-
-	table_l = fopen(file_name, "rt");
-
-	while (!feof(table_l)) {
-		if (fgetc(table_l) == '\n')
-			size += 1;
-	}
-
-	if (size != 12) {
-		printf("В файле указано слишком мало данных, должно быть 12, а передано %d, если все верно нажмите ентер после последнего элемента в файле", size);
-		exit(0);
-	}
-
-	fseek(table_l, 0, SEEK_SET); // Нужно, чтобы переместиться к началу чтения файла
-	for (int i = 0; i < 12; i++) {
-
-		fscanf(table_l, "%d ", &ptr_array[0][i]);
-		fscanf(table_l, "%d, ", &ptr_array[1][i]);
-		fscanf(table_l, "%d, ", &ptr_array[2][i]);
-		fscanf(table_l, "%d, ", &ptr_array[3][i]);
-		fscanf(table_l, "%d, ", &ptr_array[4][i]);
-		fscanf(table_l, "%d, ", &ptr_array[5][i]);
-		fscanf(table_l, "%d, ", &ptr_array[6][i]);
-		fscanf(table_l, "%d, ", &ptr_array[7][i]);
-		fscanf(table_l, "%d, ", &ptr_array[8][i]);
-		fscanf(table_l, "%d, ", &ptr_array[9][i]);
-		fscanf(table_l, "%d, ", &ptr_array[10][i]);
-		fscanf(table_l, "%d, ", &ptr_array[11][i]);
-		fscanf(table_l, "%d, ", &ptr_array[12][i]);
-		fscanf(table_l, "%d, ", &ptr_array[13][i]);
-		fscanf(table_l, "%d\n", &ptr_array[14][i]);
-	}
-	return 1;
-}
 
 
 void main() {
@@ -281,3 +97,208 @@ void main() {
 }
 
 
+
+// Functions for add elements in array
+void id_team(int ptr_array[12]) {
+	for (int i = 0; i < 12; i++) {
+		ptr_array[i] = 10000 + rand() % (99999 - 10000 + 1);
+	}
+}
+
+void num_quest(int ptr_array[12][15]) {
+	for (int i = 1; i < 13; i++) {
+		for (int j = 0; j < 14; j++) {
+			ptr_array[i][j] = rand() % 2;
+		}
+	}
+}
+
+void num_time(int ptr_array[12][15]) {
+	for (int i = 0; i < 12; i++) {
+		ptr_array[13][i] = rand() % 300;
+	}
+}
+
+void num_volume(int ptr_array[12][15]) {
+	for (int i = 0; i < 12; i++) {
+		ptr_array[14][i] = rand() % 100;
+	}
+}
+//
+
+
+//function for sum score for teams
+int num_score(int ptr_array[12][15], int index) {
+	int id_team = ptr_array[index], num_score = 0, time_score;
+
+	for (int i = 1; i < 13; i++) {
+		num_score += ptr_array[i][index];
+	}
+
+	time_score = ptr_array[13][index] / 60;
+	num_score += time_score;
+
+	return num_score;
+}
+//
+
+
+//function for table on screen
+void table1_on_screen(int ptr_array[12][15]) {
+	printf("Id команды Зад.1 Зад.2 Зад.3 Зад.4   Зад.5 Зад.6  Зад.7   Зад.8   Зад.9 Зад.10 Зад.11 Зад.12 Ост.время Исп.памяти");
+	for (int i = 0; i < 12; i++) { //nlines
+		printf("\n");
+		for (int j = 0; j < 15; j++) { //ncols
+			if (j != 13) {
+				printf("%6d ", ptr_array[j][i]);//i * ncols + j
+			}
+			else {
+				if (ptr_array[j][i] % 60 > 9) {
+					printf("   0%d:%2d", ptr_array[j][i] / 60, ptr_array[j][i] % 60);
+				}
+				else {
+					printf("   0%d:0%d", ptr_array[j][i] / 60, ptr_array[j][i] % 60);
+				}
+			}
+		}
+	}
+	printf("\n");
+}
+
+void table2_on_screen(int ptr_array[12][15]) {
+	printf("Id команды   Кол-во баллов\n");
+	for (int i = 0; i < 12; i++) {
+		printf("%6d              %2d\n", ptr_array[0][i], num_score(ptr_array, i));
+	}
+}
+//
+
+
+//function for find tasks
+int find_quest(int ptr_array[12][15]) {
+	int num_quest, result;
+
+	printf("Введите номер задачи, по которой хотите произвести поиск (1-12): ");
+	scanf_s("%d", &num_quest);
+	printf("Была ли решена задача? (введите номер ответа):\n 1. Да \n 0. Нет\n");
+	scanf_s("%d", &result);
+
+	system("cls");
+	printf("Id команды Зад.1 Зад.2 Зад.3 Зад.4   Зад.5 Зад.6  Зад.7   Зад.8   Зад.9 Зад.10 Зад.11 Зад.12 Исп.памяти   Ост.время\n");
+	for (int i = 0; i < 12; i++) {
+
+		if (ptr_array[num_quest][i] == result) {
+			printf("%6d", ptr_array[0][i]);
+
+			for (int k = 1; k < 13; k++) {
+				printf("%6d ", ptr_array[k][i]);
+			}
+
+			printf("  %6d", ptr_array[13][i]);
+
+			if (ptr_array[14][i] % 60 > 9) {
+				printf("          0%d:%2d", ptr_array[14][i] / 60, ptr_array[14][i] % 60);
+
+			}
+			else {
+				printf("          0%d:0%0d", ptr_array[14][i] / 60, ptr_array[14][i] % 60);
+			}
+			printf("\n");
+		}
+		else {
+			continue;
+		}
+
+
+
+	}
+	printf("\n\n");
+}
+//
+
+
+//function for sort 2d array
+void sort_2d_array(int ptr_array[12][15]) {
+	int tmp;
+	int choose;
+
+	printf("Сортировка:\n1. По возрастанию\n2. По убыванию\n");
+	scanf_s("%d", &choose);
+
+	if (choose == 1) {
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 12; j++) {
+				if (ptr_array[13][j] > ptr_array[13][j + 1]) {
+					for (int k = 0; k < 15; k++) {
+						tmp = ptr_array[k][j];
+						ptr_array[k][j] = ptr_array[k][j + 1];
+						ptr_array[k][j + 1] = tmp;
+					}
+				}
+			}
+		}
+	}
+	if (choose == 2) {
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 11; j++) {
+				if (ptr_array[13][j] < ptr_array[13][j + 1]) {
+					for (int k = 0; k < 15; k++) {
+						tmp = ptr_array[k][j];
+						ptr_array[k][j] = ptr_array[k][j + 1];
+						ptr_array[k][j + 1] = tmp;
+					}
+				}
+			}
+		}
+	}
+}
+//
+ 
+
+//function for input data from file
+FILE* input_from_file(int ptr_array[12][15]) {
+	char  extension[6] = { ".txt\0" };
+	char file_name[40] = "data";
+	int size = 0;
+	FILE* table_l;
+
+
+	puts("Введите название файла в пределах 30 символов (без указания расширения файла)");
+	scanf("%s", &file_name);
+	strcat(file_name, extension);
+	printf("Имя файла: %s\n", file_name);
+
+	table_l = fopen(file_name, "rt");
+
+	while (!feof(table_l)) {
+		if (fgetc(table_l) == '\n')
+			size += 1;
+	}
+
+	if (size != 12) {
+		printf("В файле указано слишком мало данных, должно быть 12, а передано %d, если все верно нажмите ентер после последнего элемента в файле", size);
+		exit(0);
+	}
+
+	fseek(table_l, 0, SEEK_SET); // Нужно, чтобы переместиться к началу чтения файла
+	for (int i = 0; i < 12; i++) {
+
+		fscanf(table_l, "%d ", &ptr_array[0][i]);
+		fscanf(table_l, "%d, ", &ptr_array[1][i]);
+		fscanf(table_l, "%d, ", &ptr_array[2][i]);
+		fscanf(table_l, "%d, ", &ptr_array[3][i]);
+		fscanf(table_l, "%d, ", &ptr_array[4][i]);
+		fscanf(table_l, "%d, ", &ptr_array[5][i]);
+		fscanf(table_l, "%d, ", &ptr_array[6][i]);
+		fscanf(table_l, "%d, ", &ptr_array[7][i]);
+		fscanf(table_l, "%d, ", &ptr_array[8][i]);
+		fscanf(table_l, "%d, ", &ptr_array[9][i]);
+		fscanf(table_l, "%d, ", &ptr_array[10][i]);
+		fscanf(table_l, "%d, ", &ptr_array[11][i]);
+		fscanf(table_l, "%d, ", &ptr_array[12][i]);
+		fscanf(table_l, "%d, ", &ptr_array[13][i]);
+		fscanf(table_l, "%d\n", &ptr_array[14][i]);
+	}
+	return 1;
+}
+//
